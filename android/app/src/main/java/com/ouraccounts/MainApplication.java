d42e3fb54d07579629663ea504510de177410174
niabby <329128796@qq.com>
2019-06-09 17:43:33 +0800

    env

--- android/app/src/main/java/com/ouraccounts/MainApplication.java
+++ android/app/src/main/java/com/ouraccounts/MainApplication.java
@@ -3,6 +3,8 @@ package com.ouraccounts;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNScreensPackage(),
             new RNFusedLocationPackage(),
             new RNGestureHandlerPackage()
       );
