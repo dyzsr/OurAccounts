dcca4e15e026f7c64d53bb0c6049cb726cb68d86
niabby <329128796@qq.com>
2019-06-11 15:33:40 +0800

    calendar

--- src/views/Months.js
+++ src/views/Months.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    Alert
+} from 'react-native';
 
-export default class MonthsView extends React.Component {
-    render(props) {
+import MinimalismCalendar from 'react-native-ocalendar';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+
+export default class Months extends Component {
+    render() {
         return (
-            <View style={styles.mainContent}>
-                <Text>Months</Text>
-            </View>
+            <Calendar
+                onDayPress = {(day) => {
+                    console.log('selected day', day)
+                }}
+                monthFormat = { 'yyyy年M月' }
+                onMonthChange = {(month) => {
+                    console.log('month changed', month)
+                }}
+            />
         );
     }
 }
 
-const styles = StyleSheet.create({
-    mainContent: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 80,
-    },
+const mapStateToProps = ({accountInfo}) => ({accounts: accountInfo.accounts});
+
+const mapDispatchToProps = (dispatch) => ({
+    onClickAdd: () => dispatch({type: 'account_add'}),
 });
+
+const AccountsView_ = connect(mapStateToProps, mapDispatchToProps)(Accounts);
+const AccountsView = createStackNavigator({
+    accountList: {screen: AccountsView_},
+    accountEdit: {screen: AccountEditView},
+}, {
+    initialRouteKey: 'accountList',
+});
+
+export default MonthsView;
--- src/views/Statistics.js
+++ src/views/Statistics.js
@@ -1,47 +1,13 @@
-/*
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-*/
-
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import Echarts from 'native-echarts';
 
 export default class StatisticsView extends React.Component {
-    render() {
-        const option = {
-            title: {
-                text: 'ECharts demo'
-            },
-            tooltip: {},
-            legend: {
-                data:['销量']
-            },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        };
-        return (
-            <Echarts option={option} height={300} />
-        );
-        /*
+    render(props) {
         return (
             <View style={styles.mainContent}>
                 <Text>Statistics</Text>
             </View>
         );
-        */
     }
 }
 
